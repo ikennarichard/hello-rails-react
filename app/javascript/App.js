ecbec0671aa8765d553e56a4449c2b020174f90c
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-15 17:30:13 -1100

    Update routes

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,25 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Greeting from "./components/Greeting";
+import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Greeting from "./components/Greeting"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <h1>Greetings my dear friends</h1>,
-  },
-
-  {
-    path: "/greetings/greeting",
-    element: <Greeting />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/greetings/greeting" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
