6e8bb4756f51bffc4ba4d8b8b3f8eb99c6cbcd66
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-15 17:37:25 -1100

    Update display

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRandomGreeting } from '../redux/greetings/apiSlice';
 
 export default function Greeting() {
-  return (
-    <div>
-      Greeting
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const { greeting, error, isLoading } = useSelector(
+    (state) => state );
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return <p>Greeting: {greeting}</p>;
+};
+
